9345139833e9f847512516268f47f4db447316fc
Pedro Jesus <pedrojesus.cefet@gmail.com>
2024-11-22 01:35:04 -0300

    mais nullable fixes

--- GlobalKeyboardCapture.Maui/Configuration/MauiAppBuilderExtensions.cs
+++ GlobalKeyboardCapture.Maui/Configuration/MauiAppBuilderExtensions.cs
@@ -13,19 +13,28 @@ public static class MauiAppBuilderExtensions
             events.AddWindows(windows => windows
                 .OnLaunched((application, args) =>
                 {
-                    var handler = Application.Current.Handler.MauiContext.Services.GetService<ILifecycleHandler>();
+                    if (Application.Current is null)
+                        return;
+
+                    var handler = Application.Current.Handler?.MauiContext?.Services.GetService<ILifecycleHandler>();
                     handler?.OnStart();
                 }));
 #elif ANDROID
             events.AddAndroid(android => android
                 .OnResume(activity =>
                 {
-                    var handler = Application.Current.Handler.MauiContext.Services.GetService<ILifecycleHandler>();
+                    if (Application.Current is null)
+                        return;
+                    
+                    var handler = Application.Current.Handler?.MauiContext?.Services.GetService<ILifecycleHandler>();
                     handler?.OnResume();
                 })
                 .OnCreate((activity, bundle) =>
                 {
-                    var handler = Application.Current.Handler.MauiContext.Services.GetService<ILifecycleHandler>();
+                    if (Application.Current is null)
+                        return;
+                    
+                    var handler = Application.Current.Handler?.MauiContext?.Services.GetService<ILifecycleHandler>();
                     handler?.OnStart();
                 }));
 #endif
--- GlobalKeyboardCapture.Maui/Configuration/ServiceCollectionExtensions.cs
+++ GlobalKeyboardCapture.Maui/Configuration/ServiceCollectionExtensions.cs
@@ -9,7 +9,7 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddKeyboardHandling(
         this IServiceCollection services,
-        Action<KeyHandlerOptions> configure = null)
+        Action<KeyHandlerOptions>? configure = null)
     {
         var options = new KeyHandlerOptions();
         configure?.Invoke(options);
